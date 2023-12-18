e5698f829c19afb147c81aa0cc7e68ca0f025765
Sipppy <94160941+Sipppy@users.noreply.github.com>
2023-12-18 11:13:57 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCELymOSb_ffY5EIq82BoiBvtZ0WIjBv_M",
-  authDomain: "spicetea-eca4b.firebaseapp.com",
-  projectId: "spicetea-eca4b",
-  storageBucket: "spicetea-eca4b.appspot.com",
-  messagingSenderId: "409416971951",
-  databaseURL: "https://spicetea-eca4b-default-rtdb.asia-southeast1.firebasedatabase.app",
-  appId: "1:409416971951:web:f29c6035a36c6a7556465a",
-  measurementId: "G-13FGVGWD0R"
-};
+function _0x2b64(){const _0x1640b=['4548546yYUdhG','3288050bsffYK','105iJPEIW','1:409416971951:web:f29c6035a36c6a7556465a','409416971951','4604688QQNnDb','593012SHtZNm','spicetea-eca4b.appspot.com','55508iIxSQH','4GkBscr','spicetea-eca4b.firebaseapp.com','8376599MEkjSl','36IYyeen','AIzaSyCELymOSb_ffY5EIq82BoiBvtZ0WIjBv_M','5dAIfuK','G-13FGVGWD0R','30003303FRgCBb'];_0x2b64=function(){return _0x1640b;};return _0x2b64();}const _0x3ce5fe=_0x5704;(function(_0x480bbd,_0x5a4feb){const _0x5027fa=_0x5704,_0x456cf8=_0x480bbd();while(!![]){try{const _0x3a8652=-parseInt(_0x5027fa(0xf9))/0x1*(parseInt(_0x5027fa(0xf6))/0x2)+parseInt(_0x5027fa(0xf2))/0x3*(-parseInt(_0x5027fa(0xf8))/0x4)+parseInt(_0x5027fa(0xed))/0x5*(-parseInt(_0x5027fa(0xf0))/0x6)+parseInt(_0x5027fa(0xfb))/0x7+parseInt(_0x5027fa(0xf5))/0x8+parseInt(_0x5027fa(0xfc))/0x9*(-parseInt(_0x5027fa(0xf1))/0xa)+parseInt(_0x5027fa(0xef))/0xb;if(_0x3a8652===_0x5a4feb)break;else _0x456cf8['push'](_0x456cf8['shift']());}catch(_0x32f79a){_0x456cf8['push'](_0x456cf8['shift']());}}}(_0x2b64,0xb8462));function _0x5704(_0x2e9367,_0x8ca1b4){const _0x2b644b=_0x2b64();return _0x5704=function(_0x5704a9,_0x2f6dfc){_0x5704a9=_0x5704a9-0xed;let _0x1a1030=_0x2b644b[_0x5704a9];return _0x1a1030;},_0x5704(_0x2e9367,_0x8ca1b4);}const firebaseConfig={'apiKey':_0x3ce5fe(0xfd),'authDomain':_0x3ce5fe(0xfa),'projectId':'spicetea-eca4b','storageBucket':_0x3ce5fe(0xf7),'messagingSenderId':_0x3ce5fe(0xf4),'databaseURL':'https://spicetea-eca4b-default-rtdb.asia-southeast1.firebasedatabase.app','appId':_0x3ce5fe(0xf3),'measurementId':_0x3ce5fe(0xee)};
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
